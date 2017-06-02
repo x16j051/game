128f86a4cb334298432c2824b24c32b50bb2828d
WIN_NT\x16j051 <x16j051@chiba-fjb.ac.jp>
2017-06-02 12:19:50 +0900

    keyboard

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,10 +31,6 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -49,46 +45,6 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(215, 187);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(30, 21);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "↑";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(242, 214);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(30, 21);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "→";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(189, 214);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(30, 21);
-            this.button5.TabIndex = 1;
-            this.button5.Text = "←";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(215, 241);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(30, 21);
-            this.button6.TabIndex = 1;
-            this.button6.Text = "↓";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
             // timer1
             // 
             this.timer1.Enabled = true;
@@ -100,13 +56,11 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -116,10 +70,6 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Timer timer1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -60,5 +60,51 @@ namespace game
             pictureBox1.Left += movex;
             pictureBox1.Top += movey;
         }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                movey = -speed;
+                //movex = 0;
+            }else if (e.KeyCode == Keys.Down)
+            {
+                movey = speed;
+                //movex = 0;
+            }else if (e.KeyCode == Keys.Right)
+            {
+                movex = speed;
+                //movey = 0;
+            }else if (e.KeyCode == Keys.Left)
+            {
+                movex = -speed;
+                //movey = 0;
+            }
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                movey = 0;
+                //movex = 0;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                movey = 0;
+                //movex = 0;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                movex = 0;
+                //movey = 0;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                movex = 0;
+                //movey = 0;
+            }
+        }
     }
 }
